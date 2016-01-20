816837cd0bf2326c16fd0b25dd2e7464c369d0a8
Frank Black <frank@frank-X38-DS5>
2016-01-20 23:13:38 +1100

    Adventure saving mode enabled! Added saving a loading functions. Added a telegram save command. Added a telegram help command. Added lists for enemies and allies.

--- cheezbot.py
+++ cheezbot.py
@@ -2,6 +2,8 @@ import sys
 import time
 import telepot
 import re
+import json
+import textwrap
 import pprint
 
 # Done! Congratulations on your new bot.
@@ -32,11 +34,11 @@ import pprint
 
 class PersistentCollection:
     """A persistent list collection, size limited, can be printed."""
-    def __init__(self,name,modifiable,size):
+    def __init__(self, command, modifiable, size):
         self.list = list()
         self.modifiable = modifiable
         self.size = size
-        self.name = name
+        self.command = command
         #maximum item string length
         self.maxlen = 100
         #default strings for replies
@@ -63,7 +65,7 @@ class PersistentCollection:
             self.aliases['add'] = self.additem
 
     def parse(self,text):
-        print(self.name + ' parsing ' + "'" + text + "'")
+        print(self.command + ' parsing ' + "'" + text + "'")
         reply = self.stringdict['exception']
         match = re.split('\s+',text.strip(' \t\n\r'))
         cmd = match[0]  #if text is an empty string, match[0] will also be an empty string
@@ -129,31 +131,57 @@ class PersistentCollection:
     def setsize(self,size):
         self.size = size
 
+    def getcommand(self):
+        return self.command
 
-def diplo(text):
-    return "Chips! (Some chips may fail if contrary to the eater's values)."
+    def tofilestring(self):
+        filestring = json.dumps(self.list)
+        return filestring
 
-def echo(text):
-    return 'All you can hear is the echo of your own voice - ' + text
+    def fromfilestring(self,filestring):
+        self.list = json.loads(filestring)
+
+class SaveHack:
+    """A hack class to load save routines into cdict"""
+    def parse(self,text):
+        savecollections()
+        return 'Weary traveller, your progress has been recorded.'
+
+    def getname(self):
+        return '/save'
+
+class HelpHack:
+    """A hack class to print help on command"""
+    def parse(self,text):
+        return textwrap.dedent("""
+        /help - Show help for this bot.
+        /rules - Show the rules of the game.
+        /inventory - List the items in your inventory.
+        /appearance - List the items that you are wearing.
+        /titles - List the titles by which you are known.
+        /songs - List the songs you have memorised.
+        /signs - List the occult signs you know.
+        /conditions - List any conditions affecting you.
+        /allies - List your heroic companions!
+        /enemies - List your nefarious rivals!
+        """)
+
+    def getname(self):
+        return '/help'
+
 
 def initcdict():
-    cdict = dict()
-    global rulescollection
-    global inventorycollection
-    global songcollection
-    global appearancecollection
-    global titlecollection
-    global conditioncollection
-    global signcollection
+    global collectionlist
 
-    cdict['/rules'] = rulescollection
-    #cdict['/echo'] = echo
-    cdict['/inventory'] = inventorycollection
-    cdict['/songs'] = songcollection
-    cdict['/appearance'] = appearancecollection
-    cdict['/titles'] = titlecollection
-    cdict['/conditions'] = conditioncollection
-    cdict['/signs'] = signcollection
+    cdict = dict()
+    for collection in collectionlist:
+        cdict[collection.getcommand()]=collection
+
+    s = SaveHack()
+    cdict[s.getname()] = s
+    h = HelpHack()
+    cdict[h.getname()] = h
+
     return cdict
 
 
@@ -167,6 +195,31 @@ def commandsplitter(text):
             arg = text[len(cmd)+1 : len(text)]
     return {'cmd':cmd,'arg':arg}
 
+def savecollections():
+    global collectionlist
+    f = open('cheez.log','w')
+    for collection in collectionlist:
+        f.write(collection.getcommand())
+        f.write('\n')
+        f.write(collection.tofilestring())
+        f.write('\n')
+
+    f.close()
+
+def loadcollections():
+    global cdict
+    f = open('cheez.log','r')
+    line = f.readline()
+    while line != '':
+        line = line.strip(' \t\n\r')
+        if line in cdict:
+            jsonline = f.readline().strip(' \t\n\r')
+            print('Loading ' + line + ' with ' + jsonline)
+            cdict[line].fromfilestring(jsonline)
+        line = f.readline()
+
+def update():
+    savecollections()
 
 def handle(msg):
     global cdict
@@ -188,52 +241,81 @@ def handle(msg):
             bot.sendMessage(chat_id,cdict[cmd].parse(arg))
 
 # GO 8080
-rulescollection = PersistentCollection('Rules',False, 100)
+collectionlist = list()
+
+rulescollection = PersistentCollection('/rules',False, 100)
 rulescollection.additem('The player can not die.')
 rulescollection.additem('Any actor may be the #DM or the #player.')
 rulescollection.additem('An actor may not take two turns in a row.')
 rulescollection.additem("Please don't abuse cheezorkbot :(")
+collectionlist.append(rulescollection)
 
-inventorycollection = PersistentCollection('Inventory', True, 10)
+inventorycollection = PersistentCollection('/inventory', True, 10)
 inventorycollection.additem('Longsword +1')
 inventorycollection.additem('Cheese of Wounding')
+collectionlist.append(inventorycollection)
 
-songcollection = PersistentCollection('Songs', True, 5)
+songcollection = PersistentCollection('/songs', True, 5)
 songcollection.setdictitem('addprefix', 'You learned ')
 songcollection.setdictitem('removeprefix', 'You forgot ')
 songcollection.setdictitem('noaddtext','You try to hum the tune but always end up at another.')
 songcollection.setdictitem('emptytext',"You've really never been much of a musician.")
+collectionlist.append(songcollection)
 
-appearancecollection = PersistentCollection('Appearance',True,10)
+appearancecollection = PersistentCollection('/appearance',True,10)
 appearancecollection.setdictitem('addprefix','You are now wearing ')
 appearancecollection.setdictitem("removeprefix",'You remove your ')
 appearancecollection.setdictitem('emptytext',"You're not wearing anything. Cheeky!")
+collectionlist.append(appearancecollection)
 
-conditioncollection = PersistentCollection('Conditions',True,5)
+conditioncollection = PersistentCollection('/conditions',True,5)
 conditioncollection.setdictitem('addprefix','You are afflicted with ')
 conditioncollection.setdictitem('removeprefix','You are no longer afflicted with ')
 conditioncollection.setdictitem('noaddtext','You are already riddled with disease. You doubt another condition will make much difference.')
 conditioncollection.setdictitem('emptytext',"Fit as a fiddle!")
+collectionlist.append(conditioncollection)
 
-titlecollection = PersistentCollection('Titles',True,3)
+titlecollection = PersistentCollection('/titles',True,3)
 titlecollection.setdictitem('addprefix','You are now known as ')
 titlecollection.setdictitem('removeprefix','You are no longer known as ')
 titlecollection.setdictitem('noaddtext','You are known by too many names already. Any more would be confusing.')
 titlecollection.setdictitem('emptytext',"Your name is known only to yourself... and you aim to keep it that way.")
+collectionlist.append(titlecollection)
 
-signcollection = PersistentCollection('Signs',True,5)
-signcollection.setdictitem('addprefix','You learned how to make ')
-signcollection.setdictitem('removeprefix','You forgot how to make ')
-signcollection.setdictitem('removesuffix',"Let's hope you don't need it later.")
+signcollection = PersistentCollection('/signs',True,5)
+signcollection.setdictitem('addprefix','You learned how to make the sign of ')
+signcollection.setdictitem('removeprefix','You forgot how to make the sign of ')
+signcollection.setdictitem('removesuffix',"; Let's hope you don't need it later.")
 signcollection.setdictitem('noaddtext','Your hands tire and the sign slips through your grasp.')
-signcollection.setdictitem('emptytext',"The only signs you know are obscene, there is no magic in them.")
+signcollection.setdictitem('emptytext',"The only signs you know are obscene; alas, there is no magic in them.")
+collectionlist.append(signcollection)
 
+allycollection = PersistentCollection('/allies',True,5)
+allycollection.setdictitem('addprefix','You are now allied with ')
+allycollection.setdictitem('removeprefix','')
+allycollection.setdictitem('removesuffix'," is no longer your ally. It is a sad day.")
+allycollection.setdictitem('noaddtext','Your legions of followers are endless. What good is one more?')
+allycollection.setdictitem('emptytext',"You learned the hard way - the only person you can rely on is yourself.")
+collectionlist.append(allycollection)
+
+enemycollection = PersistentCollection('/enemies',True,5)
+enemycollection.setdictitem('addprefix','You are instilled with a bitter hatred of ')
+enemycollection.setdictitem('removeprefix','')
+enemycollection.setdictitem('removesuffix'," is no longer your enemy.")
+enemycollection.setdictitem('noaddtext','You should deal with your existing foes first.')
+enemycollection.setdictitem('emptytext',"A man without enemies is a man who has not ventured far.")
+collectionlist.append(enemycollection)
+
+# must init CDICT *after* making the collections
 cdict = initcdict()
+# load collections from disk
+print('Loading collections...')
+loadcollections()
+print('Loaded!')
+
 # Getting the token from command-line is better than embedding it in code,
 # because tokens are supposed to be kept secret.
 TOKEN = sys.argv[1]
-
-
 
 bot = telepot.Bot(TOKEN)
 BOTUSERNAME = bot.getMe()['username']
@@ -242,5 +324,12 @@ print('Running scripts for @' + BOTUSERNAME)
 print('Listening ...')
 
 # Keep the program running.
+i = 0
 while 1:
     time.sleep(100)
+    #very rough 10m updates 6000 * 100 = 60,000
+    #don't try and clock anything with this
+    i += 1
+    if i == 6000:
+        update()
+        i = 0
